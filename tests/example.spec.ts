5dce24c26130660bec38ded2c13afdbf9e56a323
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2025-02-02 19:10:43 +0200

    refactor

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright_01/);
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
@@ -14,5 +14,5 @@ test('get started link', async ({ page }) => {
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeHidden();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
