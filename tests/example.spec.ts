1e8837894eaedb0cc97051af446edf85661608c9
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2025-02-04 18:57:40 +0200

    refactor

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright66/);
+  await expect(page).toHaveTitle(/Playwright6/);
 });
 
 test('get started link', async ({ page }) => {
